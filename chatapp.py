69a43abfe3e9c2b93cd6057f99ed9f33a93dc585
narashimha05 <139961244+narashimha05@users.noreply.github.com>
2026-09-07 00:26:25 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -100,13 +100,13 @@ def main():
         
         st.write("---")
         st.image("img/gkj.jpg")
-        st.write("AI App created by @ Gurpreet Kaur")  # add this line to display the image
+        st.write("AI App created by @ narashimha05")  # add this line to display the image
 
 
     st.markdown(
         """
         <div style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #0E1117; padding: 15px; text-align: center;">
-            © <a href="https://github.com/gurpreetkaurjethra" target="_blank">Gurpreet Kaur Jethra</a> | Made with ❤️
+            © <a href="https://github.com/narashimha05" target="_blank">Chinnari Narashimha</a> | Made with ❤️
         </div>
         """,
         unsafe_allow_html=True
